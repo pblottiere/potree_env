6b06870dbd034ef57aec74ac93b8147b7f357153
Blottiere Paul <blottiere.paul@gmail.com>
2016-06-14 17:36:23 +0200

    update after install script

--- after_install.sh
+++ after_install.sh
@@ -3,5 +3,6 @@
 sudo echo "/usr/local/lib" > /etc/ld.so.conf
 sudo ldconfig
 
-sudo update-rc.d supervisord defaults
-sudo update-rc.d greyhound defaults
+sudo update-rc.d supervisor defaults
+sudo greyhound auto
+sudo greyhound start
